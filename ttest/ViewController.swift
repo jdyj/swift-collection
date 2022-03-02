75a7f9eaccedfb4e6aebe5609cae22df6b3c9aec
jdyj <jdyj@naver.com>
2022-03-02 23:30:44 +0900

    Initial Commit

---
+++ ttest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ttest
+//
+//  Created by 조재영 on 2022/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
